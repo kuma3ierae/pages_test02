dc29edbc62a8e868c107455e927b08c74cac7e33
kuma@gmo-cybersecurity.com <kuma@gmo-cybersecurity.com>
2024-11-14 21:58:59 +0900

    updated domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,8 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
+	site: 'https://kuma3ierae.github.io',
+	base: '/pages_test02',
 	integrations: [
 		starlight({
 			title: 'My Docs',
