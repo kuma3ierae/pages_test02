09e142ec36abd1942aed9879177c3a213d0f0423
kuma@gmo-cybersecurity.com <kuma@gmo-cybersecurity.com>
2024-11-14 21:56:48 +0900

    install

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,28 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://kuma3ierae.github.io',
-  base: '/pages_test02',
-  integrations: [starlight()],
-})
+	integrations: [
+		starlight({
+			title: 'My Docs',
+			social: {
+				github: 'https://github.com/withastro/starlight',
+			},
+			sidebar: [
+				{
+					label: 'Guides',
+					items: [
+						// Each item here is one entry in the navigation menu.
+						{ label: 'Example Guide', slug: 'guides/example' },
+					],
+				},
+				{
+					label: 'Reference',
+					autogenerate: { directory: 'reference' },
+				},
+			],
+		}),
+	],
+});
