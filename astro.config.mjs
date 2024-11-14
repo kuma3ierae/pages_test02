29fcbe3493288c1d4df9e04129e2a7d5b837cdac
kuma@gmo-cybersecurity.com <kuma@gmo-cybersecurity.com>
2024-11-14 21:45:51 +0900

    updated

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,19 +1,8 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-import wikiLinkPlugin from "remark-wiki-link-plus";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://kuma3ierae.github.io',
   base: '/pages_test02',
-  markdown: {
-    remarkPlugins: [
-      [
-        wikiLinkPlugin,
-        {
-          markdownFolder: `src/content/docs`,
-        },
-      ],
-    ],
-  },
 })
