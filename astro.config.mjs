392bf097fa89062ced8d23711fe2d4bf1532eac2
kuma@gmo-cybersecurity.com <kuma@gmo-cybersecurity.com>
2024-11-14 21:50:55 +0900

    added startlight

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,11 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import starlight from '@astrojs/starlight';
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://kuma3ierae.github.io',
   base: '/pages_test02',
+  integrations: [starlight()],
 })
